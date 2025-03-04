461fc5037c2cebf809ab7a977a9bd91bae800155
itssteffi <fushajstefi@gmail.com>
2025-03-04 11:52:24 +0100

    okkk

---
+++ src/Pyramide.java
@@ -0,0 +1,13 @@
+public abstract class Pyramide {
+    private float hoehe;
+
+    public Pyramide(float hoehe) {
+        this.hoehe = hoehe;
+    }
+
+    public float getHoehe() {
+        return hoehe;
+    }
+
+    public abstract float calcVolumen(float hoehe);
+}
