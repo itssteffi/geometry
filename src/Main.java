8a6584e11cd0b6c1eca8a50866584e649c67bcd6
AlbinMerkoshi <merkoshi777@gmail.com>
2025-03-18 11:41:55 +0100

    delete

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        KreisBasisPyramide kbp = new KreisBasisPyramide(12.222f,9.22f);
-        System.out.println(kbp.calcVolumen());
-        RechteckigePyramide rpyr = new RechteckigePyramide(10,2,5);
-        System.out.println(rpyr.calcVolumen());
 
     }
 }
